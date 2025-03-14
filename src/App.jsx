3e6b44c9d1417409bf7336235e45af033d46ccc0
Ionathan Sideras <iona8ansideras@gmail.com>
2025-03-15 00:51:36 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Hightlights from "./components/Hightlights";
+
+function App() {
+    return (
+        <main className="bg-black">
+            <Navbar /> <Hero /> <Hightlights />
+        </main>
+    );
+}
+
+export default App;
---
+++ src/components/Hightlights.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Hightlights() {
+    return <div>Hightlights</div>;
+}
